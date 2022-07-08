01bc99f80e2a233216ade5215adf813b91ccd7af
Damian <damianjimenorodriguez@gmail.com>
2022-07-08 12:39:11 +0200

    Destroy animation

--- Assets/Scripts/Model/Board.cs
+++ Assets/Scripts/Model/Board.cs
@@ -1,60 +1,68 @@
 using System.Collections.Generic;
 
-namespace Tactile.TactileMatch3Challenge.Model {
-    
-    public class Board : IBoard {
-        
+namespace Tactile.TactileMatch3Challenge.Model
+{
+    public class Board : IBoard
+    {
         private Piece[,] boardState;
         private readonly IPieceSpawner pieceSpawner;
-        public System.Action<int,int> OnRemovePiece;
-        public static Board Create(int[,] definition, IPieceSpawner pieceSpawner) {
+        public System.Action<int, int, int> OnRemovePiece;
+
+        public static Board Create(int[,] definition, IPieceSpawner pieceSpawner)
+        {
             return new Board(definition, pieceSpawner);
         }
-        
-        public int Width {
+
+        public int Width
+        {
             get { return boardState.GetLength(0); }
         }
-        
-        public int Height {
+
+        public int Height
+        {
             get { return boardState.GetLength(1); }
         }
-        
-        public Board(int[,] definition, IPieceSpawner pieceSpawner) {
-            
+
+        public Board(int[,] definition, IPieceSpawner pieceSpawner)
+        {
             this.pieceSpawner = pieceSpawner;
 
             var transposed = ArrayUtility.TransposeArray(definition);
             CreatePieces(transposed);
-            
         }
 
-        private void CreatePieces(int[,] array) {
-            
+        private void CreatePieces(int[,] array)
+        {
             var defWidth = array.GetLength(0);
             var defHeight = array.GetLength(1);
-            
-            boardState = new Piece[defWidth,defHeight];
-            
-            for (int y = 0; y < defHeight; y++) {
-                for (int x = 0; x < defWidth; x++) {
-                    CreatePiece(array[x,y], x, y);
+
+            boardState = new Piece[defWidth, defHeight];
+
+            for (int y = 0; y < defHeight; y++)
+            {
+                for (int x = 0; x < defWidth; x++)
+                {
+                    CreatePiece(array[x, y], x, y);
                 }
             }
         }
-        
-        public Piece CreatePiece(int pieceType, int x, int y) { 
-            var piece = new Piece(){type = pieceType};
+
+        public Piece CreatePiece(int pieceType, int x, int y)
+        {
+            var piece = new Piece() { type = pieceType };
             boardState[x, y] = piece;
             return piece;
         }
-        
-        public int[,] GetBoardStateAsArrayWithTypes() {
-            
+
+        public int[,] GetBoardStateAsArrayWithTypes()
+        {
             var result = new int[Width, Height];
-            
-            for (int x = 0; x < Width; x++) {
-                for (int y = 0; y < Height; y++) {
-                    var p = boardState[x,y];
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    var p = boardState[x, y];
                     result[x, y] = p != null ? p.type : -1;
                 }
             }
@@ -62,19 +70,25 @@ namespace Tactile.TactileMatch3Challenge.Model {
             return ArrayUtility.TransposeArray(result);
         }
 
-        public ResolveResult Resolve(int x, int y) {
-	        FindAndRemoveConnectedAt(x, y);
-	        return MoveAndCreatePiecesUntilFull();
+        public ResolveResult Resolve(int x, int y)
+        {
+            FindAndRemoveConnectedAt(x, y);
+            return MoveAndCreatePiecesUntilFull();
         }
 
-        public Piece GetAt(int x, int y) {
+        public Piece GetAt(int x, int y)
+        {
             return boardState[x, y];
         }
-        
-        public IEnumerable<PiecePosition> IteratePieces() {
-            for (var y = 0; y < Height; y++) {
-                for (var x = 0; x < Width; x++) {
-                    yield return new PiecePosition() {
+
+        public IEnumerable<PiecePosition> IteratePieces()
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                for (var x = 0; x < Width; x++)
+                {
+                    yield return new PiecePosition()
+                    {
                         piece = boardState[x, y],
                         pos = new BoardPos(x, y)
                     };
@@ -82,76 +96,88 @@ namespace Tactile.TactileMatch3Challenge.Model {
             }
         }
 
-        public void MovePiece(int fromX, int fromY, int toX, int toY) {
+        public void MovePiece(int fromX, int fromY, int toX, int toY)
+        {
             boardState[toX, toY] = boardState[fromX, fromY];
             boardState[fromX, fromY] = null;
         }
-        
-        public bool IsWithinBounds(int x, int y) {
-            
-            if (x < Width && y < Height && x >= 0 && y >= 0) {
+
+        public bool IsWithinBounds(int x, int y)
+        {
+            if (x < Width && y < Height && x >= 0 && y >= 0)
+            {
                 return true;
             }
             return false;
-        } 
-        
-        public void RemovePieceAt(int x, int y) {
+        }
+
+        public void RemovePieceAt(int x, int y)
+        {
             if (OnRemovePiece != null)
             {
-                OnRemovePiece(x,y);
+                OnRemovePiece(x, y, boardState[x, y].type);
             }
             boardState[x, y] = null;
         }
-        
-        public bool TryGetPiecePos(Piece piece, out int px, out int py) {
-               for (int y = 0; y < Height; y++) {
-                   for (int x = 0; x < Width; x++) {
-                       if (boardState[x, y] == piece) {
-                           px = x;
-                           py = y;
-                           return true;
-                       }
-                   }
-               }
 
-               px = -1;
-               py = -1;
-               return false;
+        public bool TryGetPiecePos(Piece piece, out int px, out int py)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (boardState[x, y] == piece)
+                    {
+                        px = x;
+                        py = y;
+                        return true;
+                    }
+                }
+            }
+
+            px = -1;
+            py = -1;
+            return false;
         }
-        
-        public List<Piece> GetConnected(int x, int y) {
+
+        public List<Piece> GetConnected(int x, int y)
+        {
             var start = GetAt(x, y);
             return SearchForConnected(start, new List<Piece>());
         }
 
-        private List<Piece> SearchForConnected(Piece piece, List<Piece> searched) {
+        private List<Piece> SearchForConnected(Piece piece, List<Piece> searched)
+        {
             int x, y;
-            if (!TryGetPiecePos(piece, out x, out y)) {
+            if (!TryGetPiecePos(piece, out x, out y))
+            {
                 return searched;
             }
 
             searched.Add(piece);
-            var neighbors = GetNeighbors(x,y);
-            
-            if (neighbors.Length == 0) {
+            var neighbors = GetNeighbors(x, y);
+
+            if (neighbors.Length == 0)
+            {
                 return searched;
             }
 
-            for (int i = 0; i < neighbors.Length; i++) {
-                
+            for (int i = 0; i < neighbors.Length; i++)
+            {
                 var neighbor = neighbors[i];
-                if (!searched.Contains(neighbor) && neighbor.type == piece.type) {
+                if (!searched.Contains(neighbor) && neighbor.type == piece.type)
+                {
                     SearchForConnected(neighbor, searched);
                 }
             }
 
             return searched;
         }
-        
-        public Piece[] GetNeighbors(int x, int y) {
 
+        public Piece[] GetNeighbors(int x, int y)
+        {
             var neighbors = new List<Piece>(4);
-            
+
             neighbors = AddNeighbor(x - 1, y, neighbors); // Left
             neighbors = AddNeighbor(x, y - 1, neighbors); // Top
             neighbors = AddNeighbor(x + 1, y, neighbors); // Right
@@ -159,99 +185,112 @@ namespace Tactile.TactileMatch3Challenge.Model {
 
             return neighbors.ToArray();
         }
-        
-        private List<Piece> AddNeighbor(int x, int y, List<Piece> neighbors) {
+
+        private List<Piece> AddNeighbor(int x, int y, List<Piece> neighbors)
+        {
             if (!IsWithinBounds(x, y)) return neighbors;
-            
-            neighbors.Add(GetAt(x,y));
+
+            neighbors.Add(GetAt(x, y));
             return neighbors;
         }
-        
-        public void FindAndRemoveConnectedAt(int x, int y) {
 
-			var connections = GetConnected(x, y);
-			if (connections.Count > 1) {
-				RemovePieces(connections);
-			}
-		}
+        public void FindAndRemoveConnectedAt(int x, int y)
+        {
+            var connections = GetConnected(x, y);
+            if (connections.Count > 1)
+            {
+                RemovePieces(connections);
+            }
+        }
 
-		public ResolveResult MoveAndCreatePiecesUntilFull() {
-			
-			var result = new ResolveResult();
-			
-			int resolveStep = 0;
-			bool moreToResolve = true;
-			
-			while (moreToResolve) {
-				moreToResolve = MovePiecesOneDownIfAble(result);
-				moreToResolve |= CreatePiecesAtTop(result, resolveStep);
-				resolveStep++;
-			}
+        public ResolveResult MoveAndCreatePiecesUntilFull()
+        {
+            var result = new ResolveResult();
 
-			return result;
-		}
+            int resolveStep = 0;
+            bool moreToResolve = true;
 
-		private void RemovePieces(List<Piece> connections) {
-			foreach (var piece in connections) {
-				int x,y;
-				if(TryGetPiecePos(piece, out x, out y)){ 
-					RemovePieceAt(x,y);
-				}
-			}
-		}
-		
-		private bool CreatePiecesAtTop(ResolveResult resolveResult, int resolveStep) {
-			var createdAnyPieces = false;
-			var y = 0;
-			for (int x = 0; x < Width; x++) {
-				if (GetAt(x, y) == null) { 
-					var piece = CreatePiece(pieceSpawner.CreateBasicPiece(), x,y);
-					createdAnyPieces = true;
-                    
-					resolveResult.changes[piece] = new ChangeInfo(){
-						CreationTime = resolveStep,
-						WasCreated = true,
-						ToPos = new BoardPos(x,y),
-						FromPos = new BoardPos(x,y-1)
-					};
-				}
-			}
+            while (moreToResolve)
+            {
+                moreToResolve = MovePiecesOneDownIfAble(result);
+                moreToResolve |= CreatePiecesAtTop(result, resolveStep);
+                resolveStep++;
+            }
 
-			return createdAnyPieces;
-		}
+            return result;
+        }
 
-		private bool MovePiecesOneDownIfAble(ResolveResult resolveResult) {
-			
-			bool movedAny = false;
-			
-			for (int y = Height - 1; y >= 1; y--) {
-				for (int x = 0; x < Width; x++) {
-					
-					var dest = GetAt(x, y);
-					if (dest != null) {
-						continue;
-					}
-					
-					var pieceToMove = GetAt(x, y - 1);
-					if (pieceToMove == null) {
-						continue;
-					}
+        private void RemovePieces(List<Piece> connections)
+        {
+            foreach (var piece in connections)
+            {
+                int x, y;
+                if (TryGetPiecePos(piece, out x, out y))
+                {
+                    RemovePieceAt(x, y);
+                }
+            }
+        }
 
-					var fromX = x;
-					var fromY = y - 1;
-					MovePiece(fromX,fromY, x, y);
-					movedAny = true;
-					
-					if(!resolveResult.changes.ContainsKey(pieceToMove)) {
-						resolveResult.changes[pieceToMove] = new ChangeInfo();
-						resolveResult.changes[pieceToMove].FromPos = new BoardPos(fromX,fromY);
-					};
-					resolveResult.changes[pieceToMove].ToPos = new BoardPos(x,y);
-					
-				}
-			}
+        private bool CreatePiecesAtTop(ResolveResult resolveResult, int resolveStep)
+        {
+            var createdAnyPieces = false;
+            var y = 0;
+            for (int x = 0; x < Width; x++)
+            {
+                if (GetAt(x, y) == null)
+                {
+                    var piece = CreatePiece(pieceSpawner.CreateBasicPiece(), x, y);
+                    createdAnyPieces = true;
 
-			return movedAny;
-		}
+                    resolveResult.changes[piece] = new ChangeInfo()
+                    {
+                        CreationTime = resolveStep,
+                        WasCreated = true,
+                        ToPos = new BoardPos(x, y),
+                        FromPos = new BoardPos(x, y - 1)
+                    };
+                }
+            }
+
+            return createdAnyPieces;
+        }
+
+        private bool MovePiecesOneDownIfAble(ResolveResult resolveResult)
+        {
+            bool movedAny = false;
+
+            for (int y = Height - 1; y >= 1; y--)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    var dest = GetAt(x, y);
+                    if (dest != null)
+                    {
+                        continue;
+                    }
+
+                    var pieceToMove = GetAt(x, y - 1);
+                    if (pieceToMove == null)
+                    {
+                        continue;
+                    }
+
+                    var fromX = x;
+                    var fromY = y - 1;
+                    MovePiece(fromX, fromY, x, y);
+                    movedAny = true;
+
+                    if (!resolveResult.changes.ContainsKey(pieceToMove))
+                    {
+                        resolveResult.changes[pieceToMove] = new ChangeInfo();
+                        resolveResult.changes[pieceToMove].FromPos = new BoardPos(fromX, fromY);
+                    };
+                    resolveResult.changes[pieceToMove].ToPos = new BoardPos(x, y);
+                }
+            }
+
+            return movedAny;
+        }
     }
 }
--- Assets/Scripts/Views/BoardRenderer.cs
+++ Assets/Scripts/Views/BoardRenderer.cs
@@ -5,146 +5,162 @@ using UnityEngine;
 using Unity.Collections;
 using System.Collections;
 
-namespace Tactile.TactileMatch3Challenge.ViewComponents {
+namespace Tactile.TactileMatch3Challenge.ViewComponents
+{
+    public class BoardRenderer : MonoBehaviour
+    {
+        [SerializeField] private PieceTypeDatabase pieceTypeDatabase;
+        [SerializeField] private VisualPiece visualPiecePrefab;
 
-	public class BoardRenderer : MonoBehaviour {
-		
-		[SerializeField] private PieceTypeDatabase pieceTypeDatabase;
-		[SerializeField] private VisualPiece visualPiecePrefab;
-		
-		private Board board;
+        private Board board;
 
-		private VisualPiece[,] VisualPieces;
+        private VisualPiece[,] VisualPieces;
 
-		public bool CanClick = true;
+        public bool CanClick = true;
 
+        public Transform[] ScoreBoardPositions;
+        public int pieces;
 
-		public void Initialize(Board board) {
-			this.board = board;
-			VisualPieces = new VisualPiece[board.Width, board.Height];
-			CenterCamera();
-			CreateVisualPiecesFromBoardState();
-			board.OnRemovePiece += RemovePieceAt;
-		}
-
-		private void CenterCamera() {
-			Camera.main.transform.position = new Vector3((board.Width-1)*0.5f,-(board.Height-1)*0.5f);
-		}
-
-		private void CreateVisualPiecesFromBoardState() {
-		
-
-			foreach (var pieceInfo in board.IteratePieces()) {
-				
-				var visualPiece = CreateVisualPiece(pieceInfo.piece);
-				visualPiece.transform.localPosition = LogicPosToVisualPos(pieceInfo.pos.x, pieceInfo.pos.y);
-				VisualPieces[pieceInfo.pos.x, pieceInfo.pos.y] = visualPiece;
-			}
-		}
-		
-		public Vector3 LogicPosToVisualPos(float x,float y) { 
-			return new Vector3(x, -y, -y);
-		}
-
-		private BoardPos ScreenPosToLogicPos(float x, float y) { 
-			
-			var worldPos = Camera.main.ScreenToWorldPoint(new Vector3(x,y,-Camera.main.transform.position.z));
-			var boardSpace = transform.InverseTransformPoint(worldPos);
-
-			return new BoardPos() {
-				x = Mathf.RoundToInt(boardSpace.x),
-				y = -Mathf.RoundToInt(boardSpace.y)
-			};
-
-		}
-
-		private VisualPiece CreateVisualPiece(Piece piece) {
-			
-			var pieceObject = Instantiate(visualPiecePrefab, transform, true);
-			var sprite = pieceTypeDatabase.GetSpriteForPieceType(piece.type);
-			pieceObject.SetSprite(sprite);
-			return pieceObject;
-			
-		}
-
-		private void DestroyVisualPieces() {
-			foreach (var visualPiece in GetComponentsInChildren<VisualPiece>()) {
-				Object.Destroy(visualPiece.gameObject);
-			}
-		}
-
-		private void HandleChanges(Dictionary<Piece, ChangeInfo> changes)
-		{
-			for (int index = 0; index < changes.Count; index++)
-			{
-				var item = changes.ElementAt(index);
-				VisualPiece visualPiece = null;
-				Vector3 to = LogicPosToVisualPos(item.Value.ToPos.x, item.Value.ToPos.y);
-				Vector3 from = LogicPosToVisualPos(item.Value.FromPos.x, item.Value.FromPos.y);
-
-				if (item.Value.WasCreated)
-                {
-					visualPiece = CreateVisualPiece(item.Key);
-					VisualPieces[item.Value.ToPos.x, item.Value.ToPos.y] = visualPiece;
-					StartCoroutine(MovePieces(visualPiece, from, to, 2, 2));
-				}
-                else
-                {
-					visualPiece = VisualPieces[item.Value.FromPos.x, item.Value.FromPos.y];
-					VisualPieces[item.Value.ToPos.x, item.Value.ToPos.y] = visualPiece;	
-				StartCoroutine(MovePieces(visualPiece, from, to,2,0));
-				}
-                
-			}
-		}
-
-		public IEnumerator MovePieces(VisualPiece piece, Vector3 From, Vector3 To, float LerpTime, float waitTime)
+        public void Initialize(Board board)
         {
-			yield return new WaitForSeconds(waitTime);
-			float elapsedTime = 0;
-            while (elapsedTime< LerpTime)
-            {
-				elapsedTime += Time.deltaTime;
-				yield return null;
-				piece.transform.localPosition = Vector3.Lerp(From, To, elapsedTime / LerpTime);
-
-			}
+            this.board = board;
+            VisualPieces = new VisualPiece[board.Width, board.Height];
+            CenterCamera();
+            CreateVisualPiecesFromBoardState();
+            board.OnRemovePiece += RemovePieceAt;
         }
-		public IEnumerator CoolDown()
-		{
-			CanClick = false;
-			yield return new WaitForSeconds(2);
-			CanClick = true;
-		}
 
-		public void RemovePieceAt(int x, int y)
-		{
-            if (VisualPieces[x,y] != null)
+        private void CenterCamera()
+        {
+            Camera.main.transform.position = new Vector3((board.Width - 1) * 0.5f, -(board.Height - 1) * 0.5f);
+        }
+
+        private void CreateVisualPiecesFromBoardState()
+        {
+            foreach (var pieceInfo in board.IteratePieces())
             {
-				Destroy(VisualPieces[x, y].gameObject);
-			}
-		}
+                var visualPiece = CreateVisualPiece(pieceInfo.piece);
+                visualPiece.transform.localPosition = LogicPosToVisualPos(pieceInfo.pos.x, pieceInfo.pos.y);
+                VisualPieces[pieceInfo.pos.x, pieceInfo.pos.y] = visualPiece;
+            }
+        }
 
+        public Vector3 LogicPosToVisualPos(float x, float y)
+        {
+            return new Vector3(x, -y, -y);
+        }
 
-		private void Update() {
-			
-			if (Input.GetMouseButtonDown(0) && CanClick) {
+        private BoardPos ScreenPosToLogicPos(float x, float y)
+        {
+            var worldPos = Camera.main.ScreenToWorldPoint(new Vector3(x, y, -Camera.main.transform.position.z));
+            var boardSpace = transform.InverseTransformPoint(worldPos);
 
-				var pos = ScreenPosToLogicPos(Input.mousePosition.x, Input.mousePosition.y);
+            return new BoardPos()
+            {
+                x = Mathf.RoundToInt(boardSpace.x),
+                y = -Mathf.RoundToInt(boardSpace.y)
+            };
+        }
 
-				if (board.IsWithinBounds(pos.x, pos.y)) {
-					ResolveResult result = board.Resolve(pos.x, pos.y);
-					HandleChanges(result.changes);
-					StartCoroutine(CoolDown());
-				}
+        private VisualPiece CreateVisualPiece(Piece piece)
+        {
+            var pieceObject = Instantiate(visualPiecePrefab, transform, true);
+            var sprite = pieceTypeDatabase.GetSpriteForPieceType(piece.type);
+            pieceObject.SetSprite(sprite);
+            return pieceObject;
+        }
 
-			}
-		}
-		
-	}
+        private void DestroyVisualPieces()
+        {
+            foreach (var visualPiece in GetComponentsInChildren<VisualPiece>())
+            {
+                Object.Destroy(visualPiece.gameObject);
+            }
+        }
 
+        public float fallInterval = 0.25f;
+        public float speed = 0.25f;
 
+        private IEnumerator HandleChanges(Dictionary<Piece, ChangeInfo> changes)
+        {
+            CanClick = false;
+            for (int index = 0; index < changes.Count; index++)
+            {
+                var item = changes.ElementAt(index);
+                VisualPiece visualPiece = null;
+                Vector3 to = LogicPosToVisualPos(item.Value.ToPos.x, item.Value.ToPos.y);
+                Vector3 from = LogicPosToVisualPos(item.Value.FromPos.x, item.Value.FromPos.y);
 
+                if (!item.Value.WasCreated)
+                {
+                    visualPiece = VisualPieces[item.Value.FromPos.x, item.Value.FromPos.y];
+                    VisualPieces[item.Value.ToPos.x, item.Value.ToPos.y] = visualPiece;
 
+                    yield return StartCoroutine(MovePieces(visualPiece, from, to));
+                }
+            }
 
+            for (int index = 0; index < changes.Count; index++)
+            {
+                var item = changes.ElementAt(index);
+                VisualPiece visualPiece = null;
+                Vector3 to = LogicPosToVisualPos(item.Value.ToPos.x, item.Value.ToPos.y);
+                Vector3 from = LogicPosToVisualPos(item.Value.FromPos.x, item.Value.FromPos.y);
+
+                if (item.Value.WasCreated)
+                {
+                    to = LogicPosToVisualPos(item.Value.ToPos.x, item.Value.ToPos.y);
+                    from = LogicPosToVisualPos(item.Value.FromPos.x, item.Value.FromPos.y);
+                    visualPiece = CreateVisualPiece(item.Key);
+                    VisualPieces[item.Value.ToPos.x, item.Value.ToPos.y] = visualPiece;
+
+                    yield return StartCoroutine(MovePieces(visualPiece, from, to));
+                }
+            }
+
+            CanClick = true;
+        }
+
+        public IEnumerator MovePieces(VisualPiece piece, Vector3 From, Vector3 To)
+        {
+            piece.transform.localPosition = From;
+
+            while (Vector3.Distance(piece.transform.localPosition, To) > 0.01f)
+            {
+                piece.transform.localPosition = Vector3.MoveTowards(piece.transform.localPosition, To, speed * Time.deltaTime);
+                yield return null;
+            }
+        }
+
+        public void RemovePieceAt(int x, int y, int type)
+        {
+            if (VisualPieces[x, y] != null)
+            {
+                StartCoroutine(RemovePiecesProcess(x, y, type));
+            }
+        }
+
+        public IEnumerator RemovePiecesProcess(int x, int y, int type)
+        {
+            VisualPiece piece = VisualPieces[x, y];
+            piece.GetComponent<SpriteRenderer>().sortingOrder = 32767;
+            yield return StartCoroutine(MovePieces(piece, LogicPosToVisualPos(x, y), ScoreBoardPositions[type].position));
+            Destroy(piece.gameObject);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && CanClick)
+            {
+                var pos = ScreenPosToLogicPos(Input.mousePosition.x, Input.mousePosition.y);
+
+                if (board.IsWithinBounds(pos.x, pos.y))
+                {
+                    ResolveResult result = board.Resolve(pos.x, pos.y);
+                    StartCoroutine(HandleChanges(result.changes));
+                }
+            }
+            pieces = transform.childCount;
+        }
+    }
 }
